d32e4a31bc4c5c94ff3b59bd3f1b9ec3ba0bcda6
pravdomil <pravdomil.toman@gmail.com>
2020-06-08 22:13:08 +0200

    utils

---
+++ src/Utils/Utils.elm
@@ -0,0 +1,14 @@
+module Utils.Utils exposing (..)
+
+{-| -}
+
+
+{-| Since Cmd.batch [reverses order of commands], we have created cmd batch that
+hopefully respects order of commands.
+
+[reverses order of commands]: https://www.reddit.com/r/elm/comments/6as1re/platformcmdbatch_executes_commands_in_reverse
+
+-}
+cmdBatch : List (Cmd msg) -> Cmd msg
+cmdBatch a =
+    a |> List.reverse |> Cmd.batch
