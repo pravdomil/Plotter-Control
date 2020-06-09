5f07489776e4351f4bf39941c659628cfb67b4de
pravdomil <pravdomil.toman@gmail.com>
2020-06-09 18:33:29 +0200

    Command.elm

---
+++ src/Utils/Command.elm
@@ -0,0 +1,66 @@
+module Utils.Command exposing (..)
+
+import String exposing (fromFloat, join)
+import Utils.Utils exposing (Point)
+
+
+{-| To define MGL-IIc commands.
+-}
+type Command
+    = IN
+    | IP0011
+    | PU
+    | PU2 Float Float
+    | PD Float Float
+
+
+{-| To define plotter step size.
+-}
+stepSize : Float
+stepSize =
+    0.025
+
+
+{-| To offset commands by distance in millimeters.
+-}
+offsetBy : ( Float, Float ) -> Command -> Command
+offsetBy ( offsetX, offsetY ) a =
+    case a of
+        PU2 x y ->
+            PU2 (x + offsetX * stepSize) (y + offsetY * stepSize)
+
+        PD x y ->
+            PD (x + offsetX * stepSize) (y + offsetY * stepSize)
+
+        _ ->
+            a
+
+
+{-| To convert commands to string.
+-}
+commandsToString : List Command -> String
+commandsToString a =
+    a
+        |> List.map commandToString
+        |> join ";"
+
+
+{-| To convert command to string.
+-}
+commandToString : Command -> String
+commandToString a =
+    case a of
+        IN ->
+            "IN"
+
+        IP0011 ->
+            "IP0,0,1,1"
+
+        PU ->
+            "PU"
+
+        PU2 x y ->
+            "PU" ++ fromFloat x ++ "," ++ fromFloat y
+
+        PD x y ->
+            "PD" ++ fromFloat x ++ "," ++ fromFloat y
