a0a7ff6857086a03158b13d2afb65cf58fbf768d
pravdomil <pravdomil.toman@gmail.com>
2020-09-08 21:27:07 +0200

    commandToString

--- src/Utils/Summa.elm
+++ src/Utils/Summa.elm
@@ -5,7 +5,7 @@ module Utils.Summa exposing (..)
 
 {-| To define summa commands.
 -}
-type Commands
+type Command
     = QUERY
       --
     | MENU
@@ -40,3 +40,75 @@ type Commands
     | UPGRADE_FLASH
       --
     | END
+
+
+{-| To convert command to string.
+-}
+commandToString : Command -> String
+commandToString a =
+    case a of
+        QUERY ->
+            "QUERY"
+
+        MENU ->
+            "MENU"
+
+        SET ->
+            "SET"
+
+        SYS_MENU ->
+            "SYS_MENU"
+
+        SETSYS ->
+            "SETSYS"
+
+        LOAD_MARKERS ->
+            "LOAD_MARKERS"
+
+        RELOAD_MARKERS ->
+            "RELOAD_MARKERS"
+
+        READ_MARKERS ->
+            "READ_MARKERS"
+
+        ACTIVATE_BARCODE ->
+            "ACTIVATE_BARCODE"
+
+        READ_MEDIA_SENSORS ->
+            "READ_MEDIA_SENSORS"
+
+        SWEEP_MEDIA_SENSORS ->
+            "SWEEP_MEDIA_SENSORS"
+
+        READ_LAST_FILE ->
+            "READ_LAST_FILE"
+
+        RECUT ->
+            "RECUT"
+
+        SET_ORIGIN ->
+            "SET_ORIGIN"
+
+        STORE ->
+            "STORE"
+
+        RESTORE ->
+            "RESTORE"
+
+        READ_LANGUAGE ->
+            "READ_LANGUAGE"
+
+        WRITE_LANGUAGE ->
+            "WRITE_LANGUAGE"
+
+        SET_SERIAL ->
+            "SET_SERIAL"
+
+        CAL_AXES_LOGI2 ->
+            "CAL_AXES_LOGI2"
+
+        UPGRADE_FLASH ->
+            "UPGRADE_FLASH"
+
+        END ->
+            "END"
