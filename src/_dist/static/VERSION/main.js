4ab0c555d29b726e64f36b461be786f38ab26140
pravdomil <pravdomil.toman@gmail.com>
2020-11-23 23:09:44 +0100

    dist folder

--- src/_dist/static/VERSION/main.js
+++ src/_dist/static/VERSION/main.js
@@ -1,1 +1,15 @@
-System.import("_main/main")
+main()
+
+function main() {
+  const app = Elm.Main.init({
+    node: element(document.body, "div"),
+    flags: {},
+  })
+}
+
+function element(parent, type, className) {
+  const el = document.createElement(type)
+  if (parent) parent.appendChild(el)
+  if (className) el.className = className
+  return el
+}
