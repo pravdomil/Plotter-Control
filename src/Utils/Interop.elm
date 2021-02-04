993ca021ad8522c60823bcfe0c36eb1138464dea
pravdomil <pravdomil.toman@gmail.com>
2021-02-04 20:38:26 +0100

    Interop module

--- src/Utils/Interop.elm
+++ src/Utils/Interop.elm
@@ -1,31 +1,24 @@
 port module Utils.Interop exposing (..)
 
-import Generated.Types.MessagesDecode exposing (javaScriptMessageDecoder)
-import Generated.Types.MessagesEncode exposing (encodeElmMessage)
-import Json.Decode as Decode exposing (decodeValue)
-import Json.Encode as Encode
-import Types.Messages exposing (ElmMessage, JavaScriptMessage)
+import Json.Decode as Decode
 
 
-{-| To send Elm message.
--}
-port sendElmMessage_ : Encode.Value -> Cmd msg
+{-| -}
+port sendData : String -> Cmd msg
 
 
-{-| To send Elm message.
--}
-sendElmMessage : ElmMessage -> Cmd msg
-sendElmMessage a =
-    a |> encodeElmMessage |> sendElmMessage_
+{-| -}
+port statusSubscription : (Decode.Value -> msg) -> Sub msg
 
 
-{-| To subscribe for JavaScript messages.
--}
-port javaScriptMessageSubscription_ : (Decode.Value -> msg) -> Sub msg
+
+--
 
 
-{-| To subscribe for JavaScript messages.
--}
-javaScriptMessageSubscription : (Maybe JavaScriptMessage -> msg) -> Sub msg
-javaScriptMessageSubscription toMsg =
-    javaScriptMessageSubscription_ (\v -> v |> decodeValue javaScriptMessageDecoder |> Result.toMaybe |> toMsg)
+{-| -}
+type Status
+    = Ready
+    | Connecting
+    | Idle
+    | Busy
+    | Error String
