adcc6f29fc43d967f7379cdca60d4b2b77d28282
pravdomil <pravdomil.toman@gmail.com>
2021-02-04 20:52:52 +0100

    status decoder

--- src/Utils/Interop.elm
+++ src/Utils/Interop.elm
@@ -1,6 +1,6 @@
 port module Utils.Interop exposing (..)
 
-import Json.Decode as Decode
+import Json.Decode as Decode exposing (Decoder)
 
 
 {-| -}
@@ -22,3 +22,30 @@ type Status
     | Idle
     | Busy
     | Error String
+
+
+{-| -}
+statusDecoder : Decoder Status
+statusDecoder =
+    Decode.field "_" Decode.int
+        |> Decode.andThen
+            (\i___ ->
+                case i___ of
+                    0 ->
+                        Decode.succeed Ready
+
+                    1 ->
+                        Decode.succeed Connecting
+
+                    2 ->
+                        Decode.succeed Idle
+
+                    3 ->
+                        Decode.succeed Busy
+
+                    4 ->
+                        Decode.map Error (Decode.field "a" Decode.string)
+
+                    _ ->
+                        Decode.fail ("I can't decode " ++ "Status" ++ ", unknown variant with index " ++ String.fromInt i___ ++ ".")
+            )
