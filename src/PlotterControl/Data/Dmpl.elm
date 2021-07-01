e11f75152e2c5eda341be32ab0842da06f68c474
pravdomil <pravdomil.toman@gmail.com>
2021-07-01 18:35:51 +0200

    Dmpl

--- src/PlotterControl/Data/Dmpl.elm
+++ src/PlotterControl/Data/Dmpl.elm
@@ -1,4 +1,54 @@
 module PlotterControl.Data.Dmpl exposing (..)
 
+import Parser as P exposing ((|.), (|=), Parser)
+
+
 {-| <https://en.wikipedia.org/wiki/DMPL>
 -}
+type alias Dmpl =
+    List Command
+
+
+type Command
+    = Home
+    | ResetOrigin
+    | AbsolutePositioning
+    | CoordinateResolution
+    | PenDown
+    | PenUp
+    | MoveTo Int Int
+
+
+fromString : String -> Result (List P.DeadEnd) Dmpl
+fromString a =
+    P.run parser a
+
+
+toString : Dmpl -> String
+toString a =
+    ";:" ++ (List.map commandToString a |> String.join ",") ++ "@"
+
+
+commandToString : Command -> String
+commandToString a =
+    case a of
+        Home ->
+            "H"
+
+        ResetOrigin ->
+            "O"
+
+        AbsolutePositioning ->
+            "A"
+
+        CoordinateResolution ->
+            "ECN"
+
+        PenDown ->
+            "D"
+
+        PenUp ->
+            "U"
+
+        MoveTo b c ->
+            String.fromInt b ++ "," ++ String.fromInt c
