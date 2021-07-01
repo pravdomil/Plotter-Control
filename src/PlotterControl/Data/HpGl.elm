349b7f6f7b78cafd125ac8d445a8e7ae002246c5
pravdomil <pravdomil.toman@gmail.com>
2021-07-01 13:35:29 +0200

    HpGl

--- src/PlotterControl/Data/HpGl.elm
+++ src/PlotterControl/Data/HpGl.elm
@@ -6,8 +6,8 @@ import PlotterControl.Data.PlotData as PlotData exposing (PlotData)
 
 {-| <https://en.wikipedia.org/wiki/HP-GL>
 -}
-type HpGl
-    = HpGl String
+type alias HpGl =
+    List Command
 
 
 type Command
@@ -17,24 +17,9 @@ type Command
     | PenDown (List Float)
 
 
-fromString : String -> HpGl
-fromString =
-    HpGl
-
-
-toString : HpGl -> String
-toString (HpGl a) =
-    a
-
-
-toPlotData : HpGl -> PlotData
-toPlotData (HpGl a) =
-    PlotData.fromString a
-
-
-append : HpGl -> HpGl -> HpGl
-append (HpGl a) (HpGl b) =
-    a ++ b |> HpGl
+fromString : String -> Result (List P.DeadEnd) HpGl
+fromString a =
+    P.run parser a
 
 
 
