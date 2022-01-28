710eccd01ffbe80b7cf3c4995a37f7666d4cebf5
pravdomil <pravdomil.toman@gmail.com>
2022-01-28 20:31:42 +0100

    next

--- src/PlotterControl/File.elm
+++ src/PlotterControl/File.elm
@@ -87,11 +87,116 @@ type alias Markers =
     }
 
 
+filterMarkers :
+    List (Polyline2d.Polyline2d Length.Meters ())
+    -> Result Error ( List (Polyline2d.Polyline2d Length.Meters ()), Maybe Markers )
+filterMarkers a =
+    let
+        isFirstMark : Polyline2d.Polyline2d Length.Meters coordinates -> Bool
+        isFirstMark b =
+            b
+                |> Polyline2d.boundingBox
+                |> Maybe.map
+                    (\v ->
+                        boxHasSameSizeAsMarker v
+                            && (v |> BoundingBox2d.minX |> Quantity.equalWithin (Length.millimeters 0.1) Quantity.zero)
+                            && (v |> BoundingBox2d.minY |> Quantity.equalWithin (Length.millimeters 0.1) Quantity.zero)
+                    )
+                |> Maybe.withDefault False
+    in
+    if a |> List.any isFirstMark then
+        filterMarkersHelper a
+
+    else
+        Ok
+            ( a
+            , Nothing
+            )
+
+
+filterMarkersHelper :
+    List (Polyline2d.Polyline2d Length.Meters coordinates)
+    -> Result Error ( List (Polyline2d.Polyline2d Length.Meters coordinates), Maybe { xDistance : Quantity.Quantity Float Length.Meters, yDistance : Quantity.Quantity Float Length.Meters, count : Int } )
+filterMarkersHelper a =
+    let
+        box : BoundingBox2d.BoundingBox2d Length.Meters coordinates
+        box =
+            a
+                |> List.concatMap Polyline2d.vertices
+                |> BoundingBox2d.hullN
+                |> Maybe.withDefault
+                    (BoundingBox2d.fromExtrema
+                        { minX = Quantity.zero
+                        , maxX = Quantity.zero
+                        , minY = Quantity.zero
+                        , maxY = Quantity.zero
+                        }
+                    )
+
+        isMark : Polyline2d.Polyline2d Length.Meters coordinates -> Bool
+        isMark b =
+            b
+                |> Polyline2d.boundingBox
+                |> Maybe.map
+                    (\v ->
+                        boxHasSameSizeAsMarker v
+                            && ((v |> BoundingBox2d.minY |> Quantity.equalWithin tolerance (box |> BoundingBox2d.minY))
+                                    || (v |> BoundingBox2d.maxY |> Quantity.equalWithin tolerance (box |> BoundingBox2d.maxY))
+                               )
+                    )
+                |> Maybe.withDefault False
+
+        ( markers, lines ) =
+            a |> List.partition isMark
+
+        markersCount : Int
+        markersCount =
+            markers |> List.length
+
+        markers_ : Markers
+        markers_ =
+            { xDistance =
+                box
+                    |> BoundingBox2d.dimensions
+                    |> Tuple.first
+                    |> Quantity.minus markerSize
+                    |> Quantity.divideBy (toFloat (markersCount // 2 - 1))
+            , yDistance =
+                box
+                    |> BoundingBox2d.dimensions
+                    |> Tuple.second
+                    |> Quantity.minus markerSize
+            , count = markersCount // 2
+            }
+    in
+    if markersCount < 4 || modBy 2 markersCount == 1 then
+        Err InvalidMarkerCount
+
+    else
+        Ok
+            ( lines
+            , Just markers_
+            )
+
+
 markerSize : Length.Length
 markerSize =
     Length.millimeters 3
 
 
+tolerance : Length.Length
+tolerance =
+    Quantity.float 1 |> Quantity.at_ HP_GL.resolution
+
+
+boxHasSameSizeAsMarker : BoundingBox2d.BoundingBox2d Length.Meters coordinates -> Bool
+boxHasSameSizeAsMarker b =
+    b
+        |> BoundingBox2d.dimensions
+        |> (\( w, h ) ->
+                (w |> Quantity.equalWithin tolerance markerSize)
+                    && (h |> Quantity.equalWithin tolerance markerSize)
+           )
 
 
 
