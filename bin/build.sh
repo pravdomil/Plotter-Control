0f4ecf90c360c5e3d9b927b394e8f58d9f10592f
pravdomil <pravdomil.toman@gmail.com>
2022-01-29 14:08:42 +0100

    use elm-ffi

--- bin/build.sh
+++ bin/build.sh
@@ -27,6 +27,7 @@ sed -i "" "s/VERSION/$VERSION/g" dist/index.html
 
 # Compile application.
 elm make src/Main.elm --output "dist/static/$VERSION/elm.js" --optimize
+elm-ffi "dist/static/$VERSION/elm.js"
 {
   uglifyjs "dist/static/$VERSION/elm.js" --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,passes=2,unsafe_comps,unsafe'
   uglifyjs "dist/static/$VERSION/main.js" --compress
