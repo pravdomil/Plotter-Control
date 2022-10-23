0ede738c1ccaf5048a459529dccdaf05592827f9
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-10-23 17:11:35 +0200

    next

--- src/PlotterControl/Queue.elm
+++ src/PlotterControl/Queue.elm
@@ -2,6 +2,7 @@ module PlotterControl.Queue exposing (..)
 
 import Dict.Any
 import Id
+import Task
 import Time
 
 
@@ -9,10 +10,44 @@ type alias Queue =
     Dict.Any.Dict (Id.Id Item) Item
 
 
+
+--
+
+
 type alias Item =
-    { name : String
+    { name : ItemName
     , data : String
 
     --
     , created : Time.Posix
     }
+
+
+createItem : ItemName -> String -> Task.Task x Item
+createItem name data =
+    Task.map
+        (\x ->
+            Item
+                name
+                data
+                x
+        )
+        Time.now
+
+
+
+--
+
+
+type ItemName
+    = ItemName String
+
+
+stringToItemName : String -> ItemName
+stringToItemName =
+    ItemName
+
+
+itemNameToString : ItemName -> String
+itemNameToString (ItemName a) =
+    a
