cc83e5130e488e1b864215bd5489a9aa0cf93804
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-10-23 18:05:32 +0200

    next

---
+++ src/PlotterControl/File/View.elm
@@ -0,0 +1,80 @@
+module PlotterControl.File.View exposing (..)
+
+import Element.PravdomilUi exposing (..)
+import PlotterControl.Directory.Utils
+import PlotterControl.File
+import PlotterControl.Model
+import PlotterControl.Msg
+import PlotterControl.Settings.View
+import PlotterControl.Utils.Theme exposing (..)
+import PlotterControl.Utils.Utils
+import PlotterControl.Utils.View
+
+
+view : PlotterControl.Model.Model -> Element PlotterControl.Msg.Msg
+view model =
+    column [ width fill, spacing 16, padding 16 ]
+        [ heading1 theme
+            []
+            [ text "File"
+            ]
+        , case PlotterControl.Directory.Utils.activeFile model of
+            Just ( name, a ) ->
+                case a.ready of
+                    Ok ready ->
+                        column [ spacing 32 ]
+                            [ button theme
+                                []
+                                { label = text "Add to Queue"
+                                , onPress = Just (PlotterControl.Msg.AddFileToQueueRequested name)
+                                }
+                            , markers name ready
+                            , PlotterControl.Settings.View.view model name ready.settings
+                            ]
+
+                    Err b ->
+                        statusParagraph theme
+                            []
+                            [ case b of
+                                PlotterControl.File.FileNotSupported ->
+                                    text ("Only " ++ PlotterControl.File.supportedExtension ++ " files are supported.")
+
+                                PlotterControl.File.InvalidMarkerCount ->
+                                    text "Failed to load markers."
+
+                                PlotterControl.File.ParserError _ ->
+                                    text "Failed to parse file."
+                            ]
+
+            Nothing ->
+                statusParagraph theme
+                    []
+                    [ text "No file selected."
+                    ]
+        ]
+
+
+markers : PlotterControl.File.Name -> PlotterControl.File.Ready -> Element PlotterControl.Msg.Msg
+markers name a =
+    PlotterControl.Utils.View.twoRows
+        (text "Markers:")
+        (case a.markers of
+            Just b ->
+                row [ spacing 8 ]
+                    [ text
+                        ([ b.count |> String.fromInt
+                         , b.yDistance |> PlotterControl.Utils.Utils.mmToString
+                         , b.xDistance |> PlotterControl.Utils.Utils.mmToString
+                         ]
+                            |> String.join " × "
+                        )
+                    , textButton theme
+                        []
+                        { label = text "Test"
+                        , onPress = Just (PlotterControl.Msg.MarkerTestRequested name)
+                        }
+                    ]
+
+            Nothing ->
+                text "None"
+        )
