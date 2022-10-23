6c3d788fc8b298e6c8d1b399aed2a1d1464af5fc
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-10-23 12:03:12 +0200

    next

--- src/PlotterControl/Utils/Theme.elm
+++ src/PlotterControl/Utils/Theme.elm
@@ -1,13 +1,11 @@
 module PlotterControl.Utills.Theme exposing (..)
 
-import Element.PravdomilUI exposing (..)
-import Element.PravdomilUI.Theme.Light
+import Element.PravdomilUi.Theme.Basic
+
+
+style =
+    Element.PravdomilUi.Theme.Basic.style
 
 
 theme =
-    Element.PravdomilUI.Theme.Light.theme Element.PravdomilUI.Theme.Light.style
-
-
-dangerBackground : Attribute msg
-dangerBackground =
-    bgColor Element.PravdomilUI.Theme.Light.style.danger
+    Element.PravdomilUi.Theme.Basic.theme style
