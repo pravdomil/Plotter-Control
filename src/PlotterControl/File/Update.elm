1bec75edb45ee26a17551c86ee3a1e5e08e4636a
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-10-23 15:21:10 +0200

    next

--- src/PlotterControl/File/Update.elm
+++ src/PlotterControl/File/Update.elm
@@ -3,13 +3,16 @@ module PlotterControl.File.Update exposing (..)
 import File
 import File.Select
 import HpGl
+import Length
 import Platform.Extra
 import PlotterControl.File
 import PlotterControl.Model
 import PlotterControl.Msg
 import PlotterControl.Plotter.Utils
 import PlotterControl.Settings
+import PlotterControl.Settings.Utils
 import Process
+import Quantity
 import SummaEl
 import Task
 
@@ -79,3 +82,44 @@ sendFile model =
 
         Err _ ->
             Platform.Extra.noOperation model
+
+
+
+--
+
+
+changePreset : PlotterControl.Settings.Preset -> PlotterControl.Model.Model -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
+changePreset a model =
+    ( { model
+        | settings = model.settings |> (\x -> { x | preset = a })
+      }
+    , Cmd.none
+    )
+        |> Platform.Extra.andThen PlotterControl.Settings.Utils.configurePlotter
+
+
+changeCopies : PlotterControl.Settings.Copies -> PlotterControl.Model.Model -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
+changeCopies a model =
+    ( { model
+        | settings = (\x -> { x | copies = x.copies |> PlotterControl.Settings.copiesPlus a }) model.settings
+      }
+    , Cmd.none
+    )
+
+
+changeCopyDistance : Length.Length -> PlotterControl.Model.Model -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
+changeCopyDistance a model =
+    ( { model
+        | settings = (\x -> { x | copyDistance = x.copyDistance |> Quantity.plus a |> Quantity.max (Length.millimeters 0) }) model.settings
+      }
+    , Cmd.none
+    )
+
+
+changeMarkerLoading : PlotterControl.Settings.MarkerLoading -> PlotterControl.Model.Model -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
+changeMarkerLoading a model =
+    ( { model
+        | settings = model.settings |> (\x -> { x | markerLoading = a })
+      }
+    , Cmd.none
+    )
