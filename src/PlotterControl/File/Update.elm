1cb35f252e6bd2d3234cf98a7b1a971d1057745c
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-10-23 17:56:02 +0200

    next

--- src/PlotterControl/File/Update.elm
+++ src/PlotterControl/File/Update.elm
@@ -2,11 +2,71 @@ module PlotterControl.File.Update exposing (..)
 
 import Dict.Any
 import Length
+import Platform.Extra
+import PlotterControl.Directory.Utils
 import PlotterControl.File
 import PlotterControl.Model
 import PlotterControl.Msg
+import PlotterControl.Queue
+import PlotterControl.Queue.Update
 import PlotterControl.Settings
 import Quantity
+import SummaEl
+
+
+addFileToQueue : PlotterControl.File.Name -> PlotterControl.Model.Model -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
+addFileToQueue name model =
+    case PlotterControl.Directory.Utils.readyFileByName name model of
+        Just ( _, c ) ->
+            PlotterControl.Queue.Update.createItem
+                (name |> PlotterControl.File.nameToString |> PlotterControl.Queue.stringToItemName)
+                (PlotterControl.File.readyToPlotterData c)
+                model
+
+        Nothing ->
+            Platform.Extra.noOperation model
+
+
+testMarkers : PlotterControl.File.Name -> PlotterControl.Model.Model -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
+testMarkers name model =
+    case PlotterControl.Directory.Utils.readyFileByName name model of
+        Just ( _, b ) ->
+            case b.markers of
+                Just _ ->
+                    PlotterControl.Queue.Update.createItem
+                        (name
+                            |> PlotterControl.File.nameToString
+                            |> (\x -> "Marker Test - " ++ x)
+                            |> PlotterControl.Queue.stringToItemName
+                        )
+                        (SummaEl.toString
+                            [ SummaEl.SetSettings (PlotterControl.File.readyToSettings b)
+                            , SummaEl.LoadMarkers
+                            ]
+                        )
+                        model
+
+                Nothing ->
+                    Platform.Extra.noOperation model
+
+        Nothing ->
+            Platform.Extra.noOperation model
+
+
+configurePlotter : PlotterControl.File.Name -> PlotterControl.Model.Model -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
+configurePlotter name model =
+    case PlotterControl.Directory.Utils.readyFileByName name model of
+        Just ( _, b ) ->
+            PlotterControl.Queue.Update.createItem
+                (name |> PlotterControl.File.nameToString |> (\x -> "Configure - " ++ x) |> PlotterControl.Queue.stringToItemName)
+                (SummaEl.toString
+                    [ SummaEl.SetSettings (PlotterControl.File.readyToSettings b)
+                    ]
+                )
+                model
+
+        Nothing ->
+            Platform.Extra.noOperation model
 
 
 
