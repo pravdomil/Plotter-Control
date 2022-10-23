6fd4b2ed6708167ffdbd4e1a9a25c0ac9da2fa82
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-10-23 17:14:48 +0200

    next

--- src/PlotterControl/Model/Update.elm
+++ src/PlotterControl/Model/Update.elm
@@ -15,7 +15,7 @@ import PlotterControl.Queue.Update
 init : Json.Decode.Value -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
 init _ =
     ( PlotterControl.Model.Model
-        (Err PlotterControl.Model.NoDirectory)
+        (Err ())
         Dict.Any.empty
         (Err PlotterControl.Model.Ready)
         Dict.Any.empty
