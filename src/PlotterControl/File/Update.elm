7fb88c0b3d2f28320217d78f0936e58c382e3633
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-10-23 13:00:39 +0200

    next

--- src/PlotterControl/File/Update.elm
+++ src/PlotterControl/File/Update.elm
@@ -1,8 +1,17 @@
 module PlotterControl.File.Update exposing (..)
 
+import File
 import File.Select
+import HpGl
+import Platform.Extra
+import PlotterControl.File
 import PlotterControl.Model
 import PlotterControl.Msg
+import PlotterControl.Plotter.Utils
+import PlotterControl.Settings
+import Process
+import SummaEl
+import Task
 
 
 openFile : PlotterControl.Model.Model -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
@@ -10,3 +19,63 @@ openFile model =
     ( model
     , File.Select.file [] PlotterControl.Msg.FileReceived
     )
+
+
+fileReceived : File.File -> PlotterControl.Model.Model -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
+fileReceived a model =
+    ( { model | file = Err PlotterControl.Model.Loading }
+    , Process.sleep 10
+        |> Task.andThen (\() -> File.toString a)
+        |> Task.perform (PlotterControl.Msg.FileContentReceived a)
+    )
+
+
+fileContentReceived : File.File -> String -> PlotterControl.Model.Model -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
+fileContentReceived a b model =
+    ( { model | file = PlotterControl.File.fromFile a b |> Result.mapError PlotterControl.Model.FileError }
+    , Cmd.none
+    )
+
+
+markerTestRequested : PlotterControl.Model.Model -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
+markerTestRequested model =
+    case model.file of
+        Ok b ->
+            let
+                data : String
+                data =
+                    [ model.settings |> PlotterControl.Settings.toCommands |> Tuple.first |> SummaEl.toString
+                    , b |> PlotterControl.File.toCommands |> Tuple.first |> SummaEl.toString
+                    ]
+                        |> String.join "\n"
+            in
+            PlotterControl.Plotter.Utils.sendData data model
+
+        Err _ ->
+            Platform.Extra.noOperation model
+
+
+sendFileRequested : PlotterControl.Model.Model -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
+sendFileRequested model =
+    case model.file of
+        Ok b ->
+            let
+                ( x, x2 ) =
+                    model.settings |> PlotterControl.Settings.toCommands
+
+                ( x3, x4 ) =
+                    b |> PlotterControl.File.toCommands
+
+                data : String
+                data =
+                    String.join "\n"
+                        [ x |> SummaEl.toString
+                        , x3 |> SummaEl.toString
+                        , x4 |> HpGl.toString
+                        , x2 |> SummaEl.toString
+                        ]
+            in
+            PlotterControl.Plotter.Utils.sendData data model
+
+        Err _ ->
+            Platform.Extra.noOperation model
--- src/PlotterControl/Model/Update.elm
+++ src/PlotterControl/Model/Update.elm
@@ -1,20 +1,13 @@
 module PlotterControl.Model.Update exposing (..)
 
-import File
-import HpGl
 import Json.Decode
 import Platform.Extra
-import PlotterControl.File
 import PlotterControl.File.Update
 import PlotterControl.Model
 import PlotterControl.Msg
 import PlotterControl.Plotter.Update
-import PlotterControl.Plotter.Utils
 import PlotterControl.Settings
 import PlotterControl.Settings.Update
-import Process
-import SummaEl
-import Task
 
 
 init : Json.Decode.Value -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
@@ -42,56 +35,16 @@ update msg model =
             PlotterControl.File.Update.openFile model
 
         PlotterControl.Msg.FileReceived a ->
-            ( { model | file = Err PlotterControl.Model.Loading }
-            , Process.sleep 10
-                |> Task.andThen (\() -> File.toString a)
-                |> Task.perform (PlotterControl.Msg.FileContentReceived a)
-            )
+            PlotterControl.File.Update.fileReceived a model
 
         PlotterControl.Msg.FileContentReceived a b ->
-            ( { model | file = PlotterControl.File.fromFile a b |> Result.mapError PlotterControl.Model.FileError }
-            , Cmd.none
-            )
+            PlotterControl.File.Update.fileContentReceived a b model
 
         PlotterControl.Msg.MarkerTestRequested ->
-            case model.file of
-                Ok b ->
-                    let
-                        data : String
-                        data =
-                            [ model.settings |> PlotterControl.Settings.toCommands |> Tuple.first |> SummaEl.toString
-                            , b |> PlotterControl.File.toCommands |> Tuple.first |> SummaEl.toString
-                            ]
-                                |> String.join "\n"
-                    in
-                    PlotterControl.Plotter.Utils.sendData data model
-
-                Err _ ->
-                    Platform.Extra.noOperation model
+            PlotterControl.File.Update.markerTestRequested model
 
         PlotterControl.Msg.SendFileRequested ->
-            case model.file of
-                Ok b ->
-                    let
-                        ( x, x2 ) =
-                            model.settings |> PlotterControl.Settings.toCommands
-
-                        ( x3, x4 ) =
-                            b |> PlotterControl.File.toCommands
-
-                        data : String
-                        data =
-                            String.join "\n"
-                                [ x |> SummaEl.toString
-                                , x3 |> SummaEl.toString
-                                , x4 |> HpGl.toString
-                                , x2 |> SummaEl.toString
-                                ]
-                    in
-                    PlotterControl.Plotter.Utils.sendData data model
-
-                Err _ ->
-                    Platform.Extra.noOperation model
+            PlotterControl.File.Update.sendFileRequested model
 
         PlotterControl.Msg.PresetChanged a ->
             PlotterControl.Settings.Update.changePreset a model
