d410c4e9535e186b946c7ee4067b707908b2b3a0
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-10-23 17:19:09 +0200

    next

--- src/PlotterControl/Model/View.elm
+++ src/PlotterControl/Model/View.elm
@@ -2,26 +2,23 @@ module PlotterControl.Model.View exposing (..)
 
 import Browser
 import Element.PravdomilUi exposing (..)
-import FeatherIcons
 import File
 import Html.Events
 import Json.Decode
-import Length
-import PlotterControl.File
+import PlotterControl.Checklist.View
+import PlotterControl.Directory.View
+import PlotterControl.File.View
 import PlotterControl.Model
 import PlotterControl.Msg
-import PlotterControl.Plotter
-import PlotterControl.Settings
+import PlotterControl.Queue.View
 import PlotterControl.Utils.Theme exposing (..)
-import Usb.Device
-import WakeLock
 
 
 view : PlotterControl.Model.Model -> Browser.Document PlotterControl.Msg.Msg
 view model =
     { title = "Plotter Control"
     , body =
-        [ layout theme [] (lazy viewDropArea model)
+        [ layout theme [ width fill, height fill ] (lazy viewColumns model)
         ]
     }
 
@@ -30,270 +27,27 @@ view model =
 --
 
 
-viewDropArea : PlotterControl.Model.Model -> Element PlotterControl.Msg.Msg
-viewDropArea model =
-    el
+viewColumns : PlotterControl.Model.Model -> Element PlotterControl.Msg.Msg
+viewColumns model =
+    let
+        separator : Element msg
+        separator =
+            el [ height fill, borderWidthEach 1 0 0 0, borderColor style.back50 ] none
+    in
+    row
         [ width fill
         , height fill
         , onDragOver PlotterControl.Msg.NothingHappened
         , onDrop PlotterControl.Msg.RawFilesReceived
         ]
-        (viewInterface model)
-
-
-
---
-
-
-viewInterface : PlotterControl.Model.Model -> Element PlotterControl.Msg.Msg
-viewInterface model =
-    column
-        [ alignTop
-        , centerX
-        , spacing 24
-        , padding 32
-        ]
-        [ form
-            (heading1 theme
-                []
-                [ text "Plotter"
-                ]
-            )
-            (heading1 theme
-                []
-                [ text "Control"
-                ]
-            )
-        , form
-            (el (theme.label [ width fill, fontAlignRight ]) (text "File:"))
-            (row [ spacing 8 ]
-                [ case model.file of
-                    Ok b ->
-                        paragraph theme
-                            []
-                            [ text (b.name |> (\(PlotterControl.File.Name x) -> x))
-                            ]
-
-                    Err b ->
-                        case b of
-                            PlotterControl.Model.NotAsked ->
-                                none
-
-                            PlotterControl.Model.Loading ->
-                                text "Loading..."
-
-                            PlotterControl.Model.FileError c ->
-                                case c of
-                                    PlotterControl.File.FileNotSupported ->
-                                        text "File type is not supported."
-
-                                    PlotterControl.File.InvalidMarkerCount ->
-                                        text "Failed to load markers."
-
-                                    PlotterControl.File.ParserError _ ->
-                                        text "Failed to parse file."
-                , textButton theme
-                    []
-                    { label = text "Select"
-                    , onPress = Just PlotterControl.Msg.OpenFileRequested
-                    }
-                ]
-            )
-        , form
-            (el (theme.label [ width fill, fontAlignRight ]) (text "Markers:"))
-            (case model.file |> Result.toMaybe |> Maybe.andThen .markers of
-                Just a ->
-                    row [ spacing 8 ]
-                        [ text
-                            ([ a.count |> String.fromInt
-                             , a.yDistance |> mmToString
-                             , a.xDistance |> mmToString
-                             ]
-                                |> String.join " × "
-                            )
-                        , textButton theme
-                            []
-                            { label = text "Test"
-                            , onPress = Just PlotterControl.Msg.MarkerTestRequested
-                            }
-                        ]
-
-                Nothing ->
-                    text "None"
-            )
-        , form (el (theme.label [ width fill, fontAlignRight ]) (text "Preset:"))
-            (column [ spacing 8 ]
-                [ inputRadioRow theme
-                    [ spacing 8 ]
-                    { label = labelHidden "Preset:"
-                    , options =
-                        [ inputRadioBlockOption theme [] PlotterControl.Settings.Cut (text "Cut")
-                        , inputRadioBlockOption theme [] PlotterControl.Settings.Draw (text "Draw")
-                        , inputRadioBlockOption theme [] PlotterControl.Settings.Perforate (text "Perforate")
-                        ]
-                    , selected = Just model.settings.preset
-                    , onChange = PlotterControl.Msg.PresetChanged
-                    }
-                , paragraph theme
-                    [ fontSize 14 ]
-                    [ text "Set depth, pressure, offset, velocity, sensitivity on plotter."
-                    ]
-                ]
-            )
-        , case Ok () of
-            Ok _ ->
-                form
-                    (el (theme.label [ width fill, fontAlignRight ]) (text "Marker loading:"))
-                    (inputRadioRow theme
-                        [ spacing 8 ]
-                        { label = labelHidden "Marker Loading:"
-                        , options =
-                            [ inputRadioBlockOption theme [] PlotterControl.Settings.LoadSequentially (text "Sequentially")
-                            , inputRadioBlockOption theme [] PlotterControl.Settings.LoadAllAtOnce (text "All at Once")
-                            ]
-                        , selected = Just model.settings.markerLoading
-                        , onChange = PlotterControl.Msg.MarkerLoadingChanged
-                        }
-                    )
-
-            Err _ ->
-                none
-        , inputNumber
-            { label = el (theme.label [ width fill, fontAlignRight ]) (text "Copies:")
-            , value =
-                el [ fontVariant fontTabularNumbers ]
-                    (text
-                        (model.settings.copies
-                            |> (\(PlotterControl.Settings.Copies x) -> x)
-                            |> String.fromInt
-                        )
-                    )
-            , onChange = PlotterControl.Settings.Copies >> PlotterControl.Msg.CopiesChanged
-            }
-        , inputNumber
-            { label = el (theme.label [ width fill, fontAlignRight ]) (text "Copy Gap:")
-            , value =
-                el [ fontVariant fontTabularNumbers ]
-                    (text (mmToString model.settings.copyDistance))
-            , onChange = toFloat >> Length.millimeters >> PlotterControl.Msg.CopyDistanceChanged
-            }
-        , form
-            none
-            (let
-                sendButton : Element PlotterControl.Msg.Msg
-                sendButton =
-                    case model.file of
-                        Ok _ ->
-                            button theme
-                                [ paddingXY 16 12 ]
-                                { label = text "Send File"
-                                , onPress = Just PlotterControl.Msg.SendFileRequested
-                                }
-
-                        Err _ ->
-                            none
-
-                cancelButton : Element PlotterControl.Msg.Msg
-                cancelButton =
-                    button theme
-                        [ paddingXY 16 12, bgColor style.danger ]
-                        { label = text "Stop"
-                        , onPress = Just PlotterControl.Msg.StopSendingRequested
-                        }
-             in
-             column [ spacing 8 ]
-                (case model.plotter of
-                    Ok _ ->
-                        [ cancelButton
-                        , text "Sending..."
-                        ]
-
-                    Err b ->
-                        case b of
-                            PlotterControl.Model.Ready ->
-                                [ sendButton
-                                ]
-
-                            PlotterControl.Model.Connecting ->
-                                [ text "Connecting..."
-                                ]
-
-                            PlotterControl.Model.QueueSent ->
-                                [ sendButton
-                                , text "File sent."
-                                ]
-
-                            PlotterControl.Model.PlotterError c ->
-                                [ sendButton
-                                , case c of
-                                    PlotterControl.Plotter.USBDeviceError d ->
-                                        case d of
-                                            Usb.Device.NotSupported ->
-                                                text "Your browser is not supported."
-
-                                            Usb.Device.NothingSelected ->
-                                                none
-
-                                            Usb.Device.TransferAborted ->
-                                                text "Sending has been stopped."
-
-                                            Usb.Device.JavaScriptError _ ->
-                                                text "Internal error."
-
-                                    PlotterControl.Plotter.WakeLockError d ->
-                                        case d of
-                                            WakeLock.JavaScriptError _ ->
-                                                text "Internal error."
-                                ]
-                )
-            )
-        ]
-
-
-
---
-
-
-inputNumber : { label : Element msg, value : Element msg, onChange : Int -> msg } -> Element msg
-inputNumber config =
-    form
-        config.label
-        (row [ spacing 8 ]
-            [ textButton theme
-                []
-                { label = el [] (FeatherIcons.minus |> FeatherIcons.withSize 30 |> FeatherIcons.toHtml [] |> html)
-                , onPress = config.onChange -10 |> Just
-                }
-            , textButton theme
-                []
-                { label = el [] (FeatherIcons.minus |> FeatherIcons.withSize 20 |> FeatherIcons.toHtml [] |> html)
-                , onPress = config.onChange -1 |> Just
-                }
-            , config.value
-            , textButton theme
-                []
-                { label = el [] (FeatherIcons.plus |> FeatherIcons.withSize 20 |> FeatherIcons.toHtml [] |> html)
-                , onPress = config.onChange 1 |> Just
-                }
-            , textButton theme
-                []
-                { label = el [] (FeatherIcons.plus |> FeatherIcons.withSize 30 |> FeatherIcons.toHtml [] |> html)
-                , onPress = config.onChange 10 |> Just
-                }
-            ]
+        ([ PlotterControl.Directory.View.view model
+         , PlotterControl.File.View.view model
+         , PlotterControl.Queue.View.view model
+         , PlotterControl.Checklist.View.view model
+         ]
+            |> List.map (\x -> el [ width fill, height fill, scrollbars ] x)
+            |> List.intersperse separator
         )
-
-
-
---
-
-
-form : Element msg -> Element msg -> Element msg
-form a b =
-    row [ spacing 8 ]
-        [ el [ width (px 320), fontAlignRight ] a
-        , el [ width (px 320) ] b
-        ]
 
 
 
