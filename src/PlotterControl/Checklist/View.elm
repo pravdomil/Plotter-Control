41b92d0ef04ca56943ea321c1a84db181c3a3ab3
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-10-25 18:32:12 +0200

    next

--- src/PlotterControl/Checklist/View.elm
+++ src/PlotterControl/Checklist/View.elm
@@ -15,16 +15,32 @@ view model =
             []
             [ text "Checklist"
             ]
-        , column [ width fill ]
-            (PlotterControl.Checklist.items
-                |> List.map
-                    (\x ->
-                        viewItem
-                            (model.checkList |> Dict.Any.member PlotterControl.Checklist.toComparable x)
-                            x
-                    )
-            )
+        , viewChecklist
+            model
+            (text "For Roll")
+            PlotterControl.Checklist.rollChecklist
+        , viewChecklist
+            model
+            (text "For Markers")
+            PlotterControl.Checklist.markersChecklist
+        , viewChecklist
+            model
+            (text "For Cut")
+            PlotterControl.Checklist.cutChecklist
+        , viewChecklist
+            model
+            (text "For Perforation")
+            PlotterControl.Checklist.perforationChecklist
         ]
+
+
+viewChecklist : PlotterControl.Model.Model -> Element PlotterControl.Msg.Msg -> List PlotterControl.Checklist.Item -> Element PlotterControl.Msg.Msg
+viewChecklist model label items =
+    column [ width fill ]
+        (items
+            |> List.map (\x -> viewItem (model.checkList |> Dict.Any.member PlotterControl.Checklist.toComparable x) x)
+            |> (\x -> el (theme.label []) label :: x)
+        )
 
 
 viewItem : Bool -> PlotterControl.Checklist.Item -> Element PlotterControl.Msg.Msg
