3c0bb4457f2958f6c82bf85c26e750e2db0a9442
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-11-10 12:32:53 +0100

    next

--- src/PlotterControl/Checklist/Update.elm
+++ src/PlotterControl/Checklist/Update.elm
@@ -4,11 +4,12 @@ import Dict.Any
 import PlotterControl.Checklist
 import PlotterControl.Model
 import PlotterControl.Msg
+import PlotterControl.Page
 
 
 activateChecklist : PlotterControl.Checklist.Checklist -> PlotterControl.Model.Model -> ( PlotterControl.Model.Model, Cmd PlotterControl.Msg.Msg )
 activateChecklist a model =
-    ( { model | page = Just (PlotterControl.Model.Checklist a) }
+    ( { model | page = Just (PlotterControl.Page.Checklist_ (PlotterControl.Page.Checklist a)) }
     , Cmd.none
     )
 
