981a61310a8ab7f25f3c4d6844f9c7c88c97b40a
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-11-10 13:30:06 +0100

    next

--- src/PlotterControl/Model.elm
+++ src/PlotterControl/Model.elm
@@ -17,16 +17,7 @@ type alias Model =
     , directory : Result () PlotterControl.Directory.Directory
     , queue : PlotterControl.Queue.Queue
     , plotter : Result PlotterError PlotterControl.Plotter.Plotter
-    , checklist : Checklist
-    }
-
-
-
---
-
-
-type alias Checklist =
-    { items : Dict.Any.Dict PlotterControl.Checklist.Item ()
+    , checklist : Dict.Any.Dict PlotterControl.Checklist.Item ()
     }
 
 
