d14dfed9b17364bceb1b3230186f0ecd144d67c7
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-11-17 22:03:59 +0100

    next

--- src/PlotterControl/MarkerSensitivity.elm
+++ src/PlotterControl/MarkerSensitivity.elm
@@ -7,9 +7,19 @@ type alias MarkerSensitivity =
     Quantity.Quantity Int MarkerSensitivity_
 
 
+fromInt : Int -> MarkerSensitivity
+fromInt a =
+    Quantity.unsafe a
+
+
+toInt : MarkerSensitivity -> Int
+toInt a =
+    Quantity.unwrap a
+
+
 toString : MarkerSensitivity -> String
 toString a =
-    String.fromInt (Quantity.unwrap a) ++ " %"
+    String.fromInt (toInt a) ++ " %"
 
 
 
