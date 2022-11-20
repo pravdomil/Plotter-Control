d532c82b0c01fe9bff0c196a496db28673da14ff
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-11-20 20:42:53 +0100

    next

--- src/PlotterControl/Commander.elm
+++ src/PlotterControl/Commander.elm
@@ -14,3 +14,20 @@ type alias Commander =
 type CommandType
     = Raw
     | SummaEncapsulatedLanguage
+
+
+allCommandTypes : List CommandType
+allCommandTypes =
+    [ Raw
+    , SummaEncapsulatedLanguage
+    ]
+
+
+commandTypeToName : CommandType -> String
+commandTypeToName a =
+    case a of
+        Raw ->
+            "Raw"
+
+        SummaEncapsulatedLanguage ->
+            "Summa Language"
